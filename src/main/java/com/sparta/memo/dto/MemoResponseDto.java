aa9febd797dc814714fad12b516531841e4d3871
totohoon02 <totohoon02@gmail.com>
2024-03-03 09:19:20 +0900

    .
    .

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -3,21 +3,22 @@ package com.sparta.memo.dto;
 import com.sparta.memo.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto {
     private final Long id;
     private final String username;
     private final String contents;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
-    }
-
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 }
--- src/test/java/com/sparta/memo/TransactionTest.java
+++ src/test/java/com/sparta/memo/TransactionTest.java
@@ -45,11 +45,12 @@ public class TransactionTest {
     }
 
     @Test
+    @Disabled
 //    @Transactional
 //    @Rollback(value = false)
     @DisplayName("트랜잭션 전파 테스트")
     void test3() {
-        memoRepository.createMemo(em);
+//        memoRepository.createMemo(em);
         System.out.println("테스트 test3 메서드 종료");
     }
 }
